b6d9755cb759ab91ce8a87aa978650f3669298fa
Justin Chau <chausicle@gmail.com>
2018-03-20 10:35:03 -0700

    Change shift time to hour increments

--- seeds/shifts.js
+++ seeds/shifts.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('shifts').insert([
         {id: 1, start_time: '9:00', end_time: '14:00'},
-        {id: 2, start_time: '11:00', end_time: '16:30'},
+        {id: 2, start_time: '11:00', end_time: '16:00'},
         {id: 3, start_time: '13:00', end_time: '19:00'},
       ])
     }).then(() => {
