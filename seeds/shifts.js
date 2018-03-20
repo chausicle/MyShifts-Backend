5bb91e1539c093931a6613f5ee1fb68615c9077e
Brady <vanderbannond@gmail.com>
2018-03-20 09:44:32 -0700

    added shifts migration and seed

---
+++ seeds/shifts.js
@@ -0,0 +1,16 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('shifts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('shifts').insert([
+        {id: 1, start_time: '9:00', end_time: '14:00'},
+        {id: 2, start_time: '11:00', end_time: '16:30'},
+        {id: 3, start_time: '13:00', end_time: '19:00'},
+      ])
+    }).then(() => {
+      return knex.raw(
+        `SELECT setval('shifts_id_seq', (SELECT MAX(id) FROM shifts));`
+      )
+    })
+}
