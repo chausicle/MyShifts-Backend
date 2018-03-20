20f314b1395c9bf6647c9e7c68603744bac91b54
Brady <vanderbannond@gmail.com>
2018-03-20 15:49:33 -0700

    app file setup working

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+console.log('were in app.js');
+
+app.use(cors())
+app.use(bodyParser.json())
+app.disable('x-powered-by')
+if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({ error: err })
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found' }})
+})
+
+const listener = () => console.log(`Listening on port ${port}`)
+app.listen(port, listener)
+
+module.exports = app
