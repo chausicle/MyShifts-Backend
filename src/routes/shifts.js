e7882587183ab25e2f3c4c659e93527c97ac7f2d
Betty Chow <bettychchow@gmail.com>
2018-03-21 17:46:12 -0700

    Finish backend functions for creating and deleting request for shift to be covered

--- src/routes/shifts.js
+++ src/routes/shifts.js
@@ -6,8 +6,9 @@ console.log('in routes');
 
 router.get('/', ctrl.getAll)
 router.get('/:id', ctrl. getById)
-router.post('/', ctrl.create)
-router.put('/:id', ctrl.changeShift)
+router.post('/requests', ctrl.createRequest)
+
 router.delete('/:id', ctrl.deleteShift)
+router.delete('/requests/:id', ctrl.deleteRequest)
 
 module.exports = router
