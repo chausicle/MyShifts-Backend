4373c8ebd40d5586f44409ff28d7927e60520fae
Betty Chow <bettychchow@gmail.com>
2018-03-22 14:34:54 -0700

    Add date and time columns to requests table and update see files

--- seeds/shifts.js
+++ seeds/shifts.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('shifts').insert([
-        {id: 1, date: '2018-03-21', start: '09:00'},
-        {id: 2, date: '2018-03-21', start: '13:00'},
-        {id: 3, date: '2018-03-22', start: '17:00'},
+        {id: 1, date: '2018-03-21', start: '10:00'},
+        {id: 2, date: '2018-03-21', start: '14:00'},
+        {id: 3, date: '2018-03-22', start: '18:00'},
       ])
     }).then(() => {
       return knex.raw(
