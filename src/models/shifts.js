149180563d0cf1f43446ca9f8a2e257015d09f8f
Betty Chow <bettychchow@gmail.com>
2018-03-21 17:46:12 -0700

    Finish backend functions for creating and deleting request for shift to be covered

--- src/models/shifts.js
+++ src/models/shifts.js
@@ -2,11 +2,9 @@
 const shifts = require('../../queries/main')
 
 getAll = () => {
-  console.log('were in models');
   const requests = shifts.getAll()
   return requests
     .then(result => {
-      console.log('result in models', result);
       return result
     })
 }
@@ -15,13 +13,39 @@ function getById(id) {
 
 }
 
-function create() {
+const createRequest = (body) => {
+  const errors = []
 
+  const userId = body.user_id
+  const shiftId = body.shift_id
+
+  if(!userId || !shiftId) {
+    errors.push('Both user id and shift id are required')
+    return { errors }
+  } else {
+    const newShift = shifts.createRequest(userId, shiftId)
+    return newShift
+      .then(result => {
+        return result[0]
+    })
+  }
+  
 }
 
-function changeShift(id) {
+const deleteRequest = (id) => {
+  const errors = []
 
+  if(!id) {
+    errors.push('Request id is required')
+  }
+  const deleteReq = shifts.deleteRequest(id)
+  return deleteReq
+    .then(result => {
+      return result
+    })
 }
+
+
 
 function deleteShift(id) {
 
@@ -30,7 +54,7 @@ function deleteShift(id) {
 module.exports = {
   getAll,
   getById,
-  create,
-  changeShift,
+  createRequest,
+  deleteRequest,
   deleteShift
 }
