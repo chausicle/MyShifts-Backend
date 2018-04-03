b217c7169c2345eb8cba78dbb1dda7904efffbd4
Justin Chau <chausicle@gmail.com>
2018-04-03 13:29:23 -0700

    Change push

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -6,10 +6,13 @@ const checkLogin = (req, res) => {
   const data = model.checkLogin(email, password)
   data
   .then(result => {
+    console.log('controller result = ', result);
+    if (!result) res.status(403).send( result )
     res.status(200).send( result )
   })
   .catch(err => {
-    res.status(403).send( err )
+    console.log('controller err = ', err);
+    return res.status(403).send( err )
   })
 }
 
