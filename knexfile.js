075404597cbc03ba404c56b87897b453a2fcab7e
Betty Chow <bettychchow@gmail.com>
2018-04-05 11:30:07 -0700

    Add production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://coluwchbvnrhyo:2839263e96b05ba1114f13383da4f5f958221a79b8beb3f69796f11e08f11b32@ec2-54-163-240-54.compute-1.amazonaws.com:5432/dfipqkctg81e4v'
   },
+  production: {
+    client: 'pg',
+    connection: 'postgres://coluwchbvnrhyo:2839263e96b05ba1114f13383da4f5f958221a79b8beb3f69796f11e08f11b32@ec2-54-163-240-54.compute-1.amazonaws.com:5432/dfipqkctg81e4v'
+  },
   test: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`
